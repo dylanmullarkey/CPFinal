99cc3ba38218aa278c65926b240d2bb5d46925a1
Dylan Mullarkey <DylanMullarkey10@gmail.com>
2024-12-02 10:12:19 -0500

    Create 5 User Models

---
+++ Models/UserFoods.cs
@@ -0,0 +1,15 @@
+namespace CPFinal.Models;
+
+public class UserFoods
+{
+    public int Id { get; set; }
+
+    public int User_id { get; set; }
+    
+    public string FavFruit { get; set; }
+
+    public string FavMeat { get; set; }
+
+    public string FavCuisine { get; set; }
+
+}
